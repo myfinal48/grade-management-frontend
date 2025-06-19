f83c6869457e191a5ed0cd40597de79c36df4adb
hassandogo <hassanmahamatdogo@gmail.com>
2025-06-19 12:30:26 +0100

    feat: added pdf generation
    # Conflicts:
    #	env.template

--- app/(protected)/dashboard/@admin/generate-transcripts/page.tsx
+++
@@ -1,8 +0,0 @@
-import { PDGGenerate } from "@/components/modules/dashboard/admin/generate-transcripts";
-
-export default function GenerateTranscriptsPage() {
-    return (
-        <PDGGenerate/>
-    )
-    
-}
--- services/releveService.ts
+++
@@ -1,9 +0,0 @@
-import { apiClient } from "@/lib/axios";
-import { ReleveDto } from "@/types";
-
-
-
-export const fetchReleve = async (studentId: string, niveauId: string): Promise<ReleveDto> => {
-  const response = await apiClient.get(`/releve/${studentId}/${niveauId}`);
-  return response.data;
-};
