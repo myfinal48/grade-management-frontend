9a6ab37e039904942ac3ca554a049b42806afbe6
mrvin100 <vincentyoumssi@gmail.com>
2025-06-20 16:14:13 +0100

    fix :  env variables exports from .env file

--- lib/env.ts
+++ lib/env.ts
@@ -2,10 +2,24 @@
 // Usage: getEnv().apiUrl, getEnv().baseUrl, getEnv().authUrl, getEnv().authSecret
 
 export function getEnv() {
-  return {
+  const env = {
     baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
     apiUrl: process.env.NEXT_PUBLIC_API_URL,
     authUrl: process.env.NEXTAUTH_URL,
     authSecret: process.env.NEXTAUTH_SECRET,
   };
+
+  Object.entries(env).forEach(([key, value]) => {
+    if (!value) {
+      // Only log in development mode on client, always log on server
+      if (
+        (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') ||
+        typeof window === 'undefined'
+      ) {
+        console.warn(`Environment variable for ${key} is missing or empty.`);
+      }
+    }
+  });
+
+  return env;
 }
